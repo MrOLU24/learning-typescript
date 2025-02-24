a176b16096e33e6e38f8ed0144eb80b91916cf43
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-24 23:42:13 +0100

    first step

--- hello.js
+++ hello.js
@@ -2,9 +2,9 @@
 //     console.log(`Hello ${person}, today is ${date}!`);
 //   }
 //   greet("Brendan");
-// function greet(person: string, date: Date) {
-//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-//   }
-//   greet("Maddison", new Date());
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+}
+greet("Maddison", new Date());
 var hello = "Hello World 2";
 console.log(hello);
--- hello.ts
+++ hello.ts
@@ -4,11 +4,11 @@
    
 //   greet("Brendan");
 
-// function greet(person: string, date: Date) {
-//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-//   }
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+  }
    
-//   greet("Maddison", new Date());
+  greet("Maddison", new Date());
 
 let hello:string = "Hello World 2";
 console.log(hello);
